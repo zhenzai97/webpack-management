534a3b7ed2084875febdf62d6d655c60ebccbd2c
zhengzai97 <927403398@163.com>
2021-02-05 15:34:26 +0800

    vscode 提交测试

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import 'antd/dist/antd.css';
 import React from 'react';
 import {BrowserRouter as Router, HashRouter,Route,Switch} from "react-router-dom";
-// import {Route} from 'react-router'
 import LoginPage from '@/views/login/LoginPage';
 import AdminPage from '@/views/admin/AdminPage';
 import '../src/styles/index.less'
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 
 class App extends React.Component {
     render() {
         return (
             <HashRouter>
-                <div className="main-body">
-                    <Switch>
-                        <Route path="/" exact component={LoginPage}/>
-                        <Route path="/main" exact component={AdminPage}/>
-                    </Switch>
-                </div>
+                <ConfigProvider locale={zhCN}>
+                    <div className="main-body">
+                        <Switch>
+                            <Route path="/" exact component={LoginPage}/>
+                            <Route path="/main" exact component={AdminPage}/>
+                        </Switch>
+                    </div>
+                </ConfigProvider>
             </HashRouter>
         )
     }
